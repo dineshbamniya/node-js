7fe1d4dcf648eab9a7b89530f7d0a545336f53a9
dineshbamniya <87126737+dineshbamniya@users.noreply.github.com>
2024-11-28 18:02:55 +0000

    done

--- Basics/01.js
+++ Basics/01.js
@@ -1,1 +1,8 @@
-console.log("You are most Welcome My js Friend")
+console.log("You are most Welcome My js Friend");
+let a = 200+400;
+console.log(`A + B = ${a}`);
+
+let b = 500;
+
+console.log(`The sum of A & B = ${a+b}`);
+console.log(`My name is Dinesh bamniya`);
